a771d0f5b5c0212b2aa29a15d4c98ed375d2b149
Rica <eninafel@gmail.com>
2026-08-17 17:21:41 +0800

    Update Program.cs

--- GROUP_1X/Program.cs
+++ GROUP_1X/Program.cs
@@ -1,1 +1,16 @@
+using System;
+using System.Windows.Forms;
 
+namespace GROUP_1X
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
